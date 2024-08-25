9d91babf2fde76a29190f672e73857ad3e695a5b
Arina Khokhlova <khokhlova.arina.v@gmail.com>
2024-08-25 18:19:04 +0300

    Modify gendiff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import argparse
+from gendiff.scripts import generate_diff
 
 parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
 parser.add_argument('first_file')
@@ -7,3 +8,10 @@ parser.add_argument('second_file')
 parser.add_argument('-f', '--format',  dest='format', help='set format of output')
 args = parser.parse_args()
 print(args)
+
+def main():
+    generate_diff(filepath1, filepath2)
+
+
+if __name__ == '__main__':
+    main()
