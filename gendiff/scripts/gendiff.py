bc6da99e51a3fa9aa51a4efb9fd5c40c4e931646
Arina Khokhlova <khokhlova.arina.v@gmail.com>
2024-08-26 21:16:53 +0300

    Create gendiff function

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,16 +1,37 @@
 #!/usr/bin/env python3
 import argparse
-from gendiff.scripts import generate_diff
+import json
 
-parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
-parser.add_argument('first_file')
-parser.add_argument('second_file')
-parser.add_argument('-f', '--format',  dest='format', help='set format of output')
-args = parser.parse_args()
-print(args)
+def gendiff(filepath1, filepath2):
+    file1 = json.load(open(filepath1))
+    file2 = json.load(open(filepath2))
+    all_keys = set(file1.keys()).union(set(file2.keys()))
+    result = []
+
+    for key in sorted(all_keys):
+        if key in file1 and key not in file2:
+            result.append(f"  - {key}: {file1[key]}")
+        elif key in file2 and key not in file1:
+            result.append(f"  + {key}: {file2[key]}")
+        elif file1[key] != file2[key]:
+            result.append(f"  - {key}: {file1[key]}")
+            result.append(f"  + {key}: {file2[key]}")
+        else:
+            result.append(f"    {key}: {file1[key]}")
+    
+    result_line = "\n".join(result)
+    return f"{{\n{result_line}\n}}"
+
 
 def main():
-    generate_diff(filepath1, filepath2)
+    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument('-f', '--format',  dest='format', help='set format of output')
+    args = parser.parse_args()
+    
+    diff_result = gendiff(args.first_file, args.second_file)
+    print(diff_result)
 
 
 if __name__ == '__main__':
