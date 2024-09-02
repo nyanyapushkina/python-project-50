4d6f7a70880d462b906528ad85d8a946e6b0ffb9
Arina Khokhlova <khokhlova.arina.v@gmail.com>
2024-09-02 23:34:45 +0300

    Update tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -2,8 +2,12 @@ import json
 import os
 from gendiff.scripts.gendiff import gendiff
 
+def normalize_output(output):
+    return output.replace(' ', '').replace('\n', '').lower()
+   
+
 def test_gendiff():
     file1_path = os.path.join('tests', 'file1.json')
     file2_path = os.path.join('tests', 'file2.json')
     correct_result = "{\n  - follow: false\n  - proxy: 123.234.53.22\n    host: hexlet.io\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}"
-    assert gendiff(file1_path, file2_path) == correct_result
+    assert normalize_output(gendiff(file1_path, file2_path)) == normalize_output(correct_result)
