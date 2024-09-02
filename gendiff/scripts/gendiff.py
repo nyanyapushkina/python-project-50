54a18bd824acf450a07d3ebed31ff4128933f911
Arina Khokhlova <khokhlova.arina.v@gmail.com>
2024-09-02 23:58:31 +0300

    Update gendiff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,6 +2,7 @@
 import argparse
 import json
 
+
 def gendiff(filepath1, filepath2):
     file1 = json.load(open(filepath1))
     file2 = json.load(open(filepath2))
@@ -18,18 +19,25 @@ def gendiff(filepath1, filepath2):
             result.append(f"  + {key}: {file2[key]}")
         else:
             result.append(f"    {key}: {file1[key]}")
-    
+
     result_line = "\n".join(result)
     return f"{{\n{result_line}\n}}"
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser = argparse.ArgumentParser(
+        description=(
+            'Compares two configuration files '
+            'and shows a difference.'
+        )
+    )    
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('-f', '--format',  dest='format', help='set format of output')
+    parser.add_argument('-f', '--format', 
+                        dest='format', 
+                        help='set format of output')
     args = parser.parse_args()
-    
+
     diff_result = gendiff(args.first_file, args.second_file)
     print(diff_result)
 
