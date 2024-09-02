3024eb031d2ae4ebe6e3622ca93daf8cedd489f8
Arina Khokhlova <khokhlova.arina.v@gmail.com>
2024-09-02 23:59:59 +0300

    Update gendiff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -30,11 +30,11 @@ def main():
             'Compares two configuration files '
             'and shows a difference.'
         )
-    )    
+    )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('-f', '--format', 
-                        dest='format', 
+    parser.add_argument('-f', '--format',
+                        dest='format',
                         help='set format of output')
     args = parser.parse_args()
 
