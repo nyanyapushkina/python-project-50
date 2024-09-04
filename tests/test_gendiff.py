5844e2ef517fe4bc44d4ab96d25443906a5429f3
Arina Khokhlova <khokhlova.arina.v@gmail.com>
2024-09-04 10:50:10 +0300

    Move test files to fixtures folder

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -7,7 +7,7 @@ def normalize_output(output):
        
 
 def test_gendiff():
-    file1_path = os.path.join('tests', 'file1.json')
-    file2_path = os.path.join('tests', 'file2.json')
+    file1_path = os.path.join('tests', 'fixtures/file1.json')
+    file2_path = os.path.join('tests', 'fixtures/file2.json')
     correct_result = "{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}"
     assert normalize_output(gendiff(file1_path, file2_path)) == normalize_output(correct_result)
