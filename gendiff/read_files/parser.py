501caa1517292f4c5c79ba206818ad42a5e1f0e1
Arina Khokhlova <khokhlova.arina.v@gmail.com>
2024-09-17 11:16:08 +0300

    Update parser

--- gendiff/read_files/parser.py
+++ gendiff/read_files/parser.py
@@ -4,12 +4,11 @@ from pathlib import Path
 
 def open_file(filepath):
     file_extension = Path(filepath).suffix
-    try:
-        if file_extension.lower() == '.json':
-            with open(filepath) as f:
-                return json.load(f)
-        elif file_extension.lower() in ['.yml', '.yaml']:
-            with open(filepath) as f:
-                return yaml.safe_load(f)
-        else:
-            raise ValueError("Unsupported file format")
+    if file_extension.lower() == '.json':
+        with open(filepath) as f:
+            return json.load(f)
+    elif file_extension.lower() in ['.yml', '.yaml']:
+        with open(filepath) as f:
+            return yaml.safe_load(f)
+    else:
+        raise ValueError("Unsupported file format")
