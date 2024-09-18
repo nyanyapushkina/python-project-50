6c28c897b0cf37dfd6e6b88ae2eff146a994cf18
Arina <88387361+nyanyapushkina@users.noreply.github.com>
2024-09-18 11:58:52 +0300

    Update gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3 
 
-from gendiff import generate_diff
+from gendiff.generate_diff import generate_diff
 from gendiff.read_files.argparse import parser_arg
 
 def main():
     first_file, second_file, output_format = parser_arg()
-    print(generate_diff(first_file, second_file, output_format))
+    result = generate_diff(first_file, second_file, output_format)
+    print(result)
 
 if __name__ == '__main__':
     main()
