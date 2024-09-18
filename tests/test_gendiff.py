041d655d236bad87d2c840916b528161ce4c9dba
Arina <88387361+nyanyapushkina@users.noreply.github.com>
2024-09-18 12:16:01 +0300

    Update test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,51 +1,66 @@
+import pytest
 from gendiff.generate_diff import generate_diff
 from pathlib import Path
 
-def test_diff_json_stylish():
-    file1 = get_fixture_path('file1.json')
-    file2 = get_fixture_path('file2.json')
-    expected_output = read_file(get_fixture_path('stylish_result.txt'))
-    
-    assert generate_diff(file1, file2, 'stylish') == expected_output
+def test_stylish_json():
+    path_dict1 = get_fixture_path('file1.json')
+    path_dict2 = get_fixture_path('file2.json')
+    path_result = get_fixture_path('stylish_result.txt')
 
-def test_diff_yml_stylish():
-    file1 = get_fixture_path('file1.yml')
-    file2 = get_fixture_path('file2.yml')
-    expected_output = read_file(get_fixture_path('stylish_result.txt'))
-    
-    assert generate_diff(file1, file2, 'stylish') == expected_output
+    with open(path_result) as f:
+        expected_output = f.read().strip()
 
-def test_diff_json_plain():
-    file1 = get_fixture_path('file1.json')
-    file2 = get_fixture_path('file2.json')
-    expected_output = read_file(get_fixture_path('plain_result.txt'))
-    
-    assert generate_diff(file1, file2, 'plain') == expected_output
+    assert generate_diff(path_dict1, path_dict2, 'stylish') == expected_output
 
-def test_diff_yml_plain():
-    file1 = get_fixture_path('file1.yml')
-    file2 = get_fixture_path('file2.yml')
-    expected_output = read_file(get_fixture_path('plain_result.txt'))
-    
-    assert generate_diff(file1, file2, 'plain') == expected_output
+def test_stylish_yml():
+    path_dict1 = get_fixture_path('file1.yml')
+    path_dict2 = get_fixture_path('file2.yml')
+    path_result = get_fixture_path('stylish_result.txt')
 
-def test_diff_json_json_format():
-    file1 = get_fixture_path('file1.json')
-    file2 = get_fixture_path('file2.json')
-    expected_output = read_file(get_fixture_path('json_result.json'))
-    
-    assert generate_diff(file1, file2, 'json') == expected_output
+    with open(path_result) as f:
+        expected_output = f.read().strip()
 
-def test_diff_yml_json_format():
-    file1 = get_fixture_path('file1.yml')
-    file2 = get_fixture_path('file2.yml')
-    expected_output = read_file(get_fixture_path('json_result.json'))
-    
-    assert generate_diff(file1, file2, 'json') == expected_output
+    assert generate_diff(path_dict1, path_dict2, 'stylish') == expected_output
+
+def test_plain_json():
+    path_dict1 = get_fixture_path('file1.json')
+    path_dict2 = get_fixture_path('file2.json')
+    path_result = get_fixture_path('plain_result.txt')
+
+    with open(path_result) as f:
+        expected_output = f.read().strip()
+
+    assert generate_diff(path_dict1, path_dict2, 'plain') == expected_output
+
+def test_plain_yml():
+    path_dict1 = get_fixture_path('file1.yml')
+    path_dict2 = get_fixture_path('file2.yml')
+    path_result = get_fixture_path('plain_result.txt')
+
+    with open(path_result) as f:
+        expected_output = f.read().strip()
+
+    assert generate_diff(path_dict1, path_dict2, 'plain') == expected_output
+
+def test_json():
+    path_dict1 = get_fixture_path('file1.json')
+    path_dict2 = get_fixture_path('file2.json')
+    path_result = get_fixture_path('json_result.json')
+
+    with open(path_result) as f:
+        expected_output = f.read().strip()
+
+    assert generate_diff(path_dict1, path_dict2, 'json') == expected_output
+
+def test_yml():
+    path_dict1 = get_fixture_path('file1.yml')
+    path_dict2 = get_fixture_path('file2.yml')
+    path_result = get_fixture_path('json_result.json')
+
+    with open(path_result) as f:
+        expected_output = f.read().strip()
+
+    assert generate_diff(path_dict1, path_dict2, 'json') == expected_output
 
 def get_fixture_path(file_name):
-    return Path(__file__).parent / 'fixtures' / file_name
-
-def read_file(file_path):
-    with open(file_path) as f:
-        return f.read().strip()  
+    return Path(Path(__file__).parent.absolute() / 'fixtures' / file_name)
