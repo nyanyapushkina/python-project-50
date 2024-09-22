c30dbfa0108c62d53dc5e12096164093ebc649d2
Arina Khokhlova <khokhlova.arina.v@gmail.com>
2024-09-22 13:49:40 +0300

    Update stylish

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -2,13 +2,11 @@ def stylish(diff_list):
     return generate_diff_output(diff_list)
 
 
-def calculate_indentation(depth):
-    return " " * (depth * 4 - 2)
-
-
 def format_value(value, indent):
     if isinstance(value, dict):
-        return format_dict(value, indent)
+        indent +=  '    '
+        result = format_dict(value, indent)
+        return result
     elif value is False:
         return 'false'
     elif value is True:
@@ -17,45 +15,40 @@ def format_value(value, indent):
         return 'null'
     else:
         return str(value)
+    
 
-
-def format_dict(data, indent):
+def format_dict(dict, indent):
     result = '{\n'
-    for key in data.keys():
-        value = format_value(data[key], indent + '    ')
+    for key in dict.keys():
+        value = format_value(dict[key], indent)
         result += f"{indent}  {key}: {value}\n"
-    result += indent + '}'
+    result += indent[:-2] + '}'
     return result
-
-
-def process_node(node, level):
-    indent = calculate_indentation(level + 1)
-    
-    if node['status'] == 'nested':
-        children_output = generate_diff_output(node['children'], level + 1)
-        return f"{indent}  {node['name']}: {children_output}\n"
-    
-    data = format_value(node.get('data', {}), indent)
-    
-    if node['status'] == 'not changed':
-        return f"{indent}  {node['name']}: {data}\n"
-    elif node['status'] == 'added':
-        return f"{indent}+ {node['name']}: {data}\n"
-    elif node['status'] == 'deleted':
-        return f"{indent}- {node['name']}: {data}\n"
-    elif node['status'] == 'changed':
-        data_before = format_value(node['data before'], indent)
-        data_after = format_value(node['data after'], indent)
-        return (f"{indent}- {node['name']}: {data_before}\n"
-                f"{indent}+ {node['name']}: {data_after}\n")
 
 
 def generate_diff_output(diff_list, level=0):
     result = '{\n'
+    indent = '  '
+    for i in range(level):
+        indent += '    '  
     diff_list.sort(key=lambda x: x['name'])
-
     for node in diff_list:
-        result += process_node(node, level)
-
-    result += calculate_indentation(level) + '}'
+        if node['status'] == 'nested':
+            children_output = generate_diff_output(node['children'], level + 1)
+            result += f"{indent}  {node['name']}: {children_output}\n"
+        if node['status'] == 'not changed':
+            data = format_value(node['data'], indent)
+            result += f"{indent}  {node['name']}: {data}\n"
+        elif node['status'] == 'added':
+            data = format_value(node['data'], indent)
+            result += f"{indent}+ {node['name']}: {data}\n"
+        elif node['status'] == 'deleted':
+            data = format_value(node['data'], indent)
+            result += f"{indent}- {node['name']}: {data}\n"
+        elif node['status'] == 'changed':
+            data_before = format_value(node['data before'], indent)
+            data_after = format_value(node['data after'], indent)
+            result += (f"{indent}- {node['name']}: {data_before}\n"
+                    f"{indent}+ {node['name']}: {data_after}\n")
+    result += indent[:-2] + '}'
     return result
