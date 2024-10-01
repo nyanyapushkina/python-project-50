caff2eb765ef12621cfda14aa9bbd18fea31bb52
Arina Khokhlova <khokhlova.arina.v@gmail.com>
2024-10-01 13:36:21 +0300

    Update gendiff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,12 +1,14 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
 
 from gendiff.generate_diff import generate_diff
 from gendiff.read_files.argparse import parser_arg
+
 
 def main():
     first_file, second_file, output_format = parser_arg()
     result = generate_diff(first_file, second_file, output_format)
     print(result)
+
 
 if __name__ == '__main__':
     main()
