8284eda571139869aa32ed68fd78a1a7a3b688fb
Arina Khokhlova <khokhlova.arina.v@gmail.com>
2024-10-01 13:11:30 +0300

    Update formatter

--- gendiff/formatters/formatter.py
+++ gendiff/formatters/formatter.py
@@ -1,7 +1,8 @@
 from gendiff.formatters.stylish import stylish
 from gendiff.formatters.plain import plain
 from gendiff.formatters.json_format import json_format
-   
+
+
 def format_diff(list_diff, format_name="stylish"):
     if format_name == 'stylish':
         return stylish(list_diff)
@@ -9,5 +10,7 @@ def format_diff(list_diff, format_name="stylish"):
         return plain(list_diff)
     elif format_name == 'json':
         return json_format(list_diff)
-    
-    raise ValueError('Unknown format. Only stylish, plain and json are supported.')
+
+    raise ValueError(
+    'Unknown format. Only stylish, plain and json are supported.'
+)
