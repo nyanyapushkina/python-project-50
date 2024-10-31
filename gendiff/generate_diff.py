1002618ecb7ef6c9cfe08c922898ba5e5a7da3b1
Arina Khokhlova <khokhlova.arina.v@gmail.com>
2024-10-31 22:41:03 +0300

    Add hints

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,6 +1,5 @@
 from gendiff.read_files.parser import open_file
 from gendiff.formatters.formatter import format_diff
-from typing import Callable
 
 
 def diff(dict1, dict2):
@@ -43,7 +42,9 @@ def diff(dict1, dict2):
     return result
 
 
-def generate_diff(path_file1: str, path_file2: str, format_name: str = 'stylish') -> tuple:
+def generate_diff(path_file1: str, 
+                  path_file2: str, 
+                  format_name: str = 'stylish') -> tuple:
     dict1 = open_file(path_file1)
     dict2 = open_file(path_file2)
     list_diff = diff(dict1, dict2)
