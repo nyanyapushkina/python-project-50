eac18b4d61ae0c9056e1da304d0f3e59ad92b6d3
Arina Khokhlova <khokhlova.arina.v@gmail.com>
2024-11-02 11:31:27 +0300

    Split generate diff into files

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,18 +1,6 @@
 #!/usr/bin/env python3
-
-from gendiff.formatters.formatter import format_diff
+from gendiff.generate_diff import generate_diff
 from gendiff.read_files.argparse import parser_arg
-from gendiff.read_files.parser import open_file
-from .scripts.diff import diff
-
-
-def generate_diff(path_file1: str,
-                  path_file2: str,
-                  format_name: str = 'stylish') -> tuple:
-    dict1 = open_file(path_file1)
-    dict2 = open_file(path_file2)
-    list_diff = diff(dict1, dict2)
-    return format_diff(list_diff, format_name)
 
 
 def main():
