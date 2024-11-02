f002cc595dbe1c53b741dd4259b21efa31d7ac3b
Arina Khokhlova <khokhlova.arina.v@gmail.com>
2024-11-02 10:53:47 +0300

    Move generate_diff function

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,7 +1,3 @@
-from gendiff.read_files.parser import open_file
-from gendiff.formatters.formatter import format_diff
-
-
 def diff(dict1, dict2):
     result = []
     all_keys = set(dict1.keys()).union(set(dict2.keys()))
@@ -40,12 +36,3 @@ def diff(dict1, dict2):
             })
 
     return result
-
-
-def generate_diff(path_file1: str,
-                  path_file2: str,
-                  format_name: str = 'stylish') -> tuple:
-    dict1 = open_file(path_file1)
-    dict2 = open_file(path_file2)
-    list_diff = diff(dict1, dict2)
-    return format_diff(list_diff, format_name)
