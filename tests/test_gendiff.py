c04759efcd05c10356200074ab1ef62354157e91
Arina Khokhlova <khokhlova.arina.v@gmail.com>
2024-11-08 12:27:47 +0300

    Use parametrize for tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,71 +1,24 @@
+import pytest
 from gendiff.gendiff import generate_diff
 from pathlib import Path
 
-
-def test_stylish_json():
-    path_dict1 = get_fixture_path('file1.json')
-    path_dict2 = get_fixture_path('file2.json')
-    path_result = get_fixture_path('stylish_result.txt')
-
-    with open(path_result) as f:
-        expected_output = f.read().strip()
-
-    assert generate_diff(path_dict1, path_dict2, 'stylish') == expected_output
-
-
-def test_stylish_yml():
-    path_dict1 = get_fixture_path('file1.yml')
-    path_dict2 = get_fixture_path('file2.yml')
-    path_result = get_fixture_path('stylish_result.txt')
+@pytest.mark.parametrize("file1, file2, result_file, format", [
+    ('file1.json', 'file2.json', 'stylish_result.txt', 'stylish'),
+    ('file1.yml', 'file2.yml', 'stylish_result.txt', 'stylish'),
+    ('file1.json', 'file2.json', 'plain_result.txt', 'plain'),
+    ('file1.yml', 'file2.yml', 'plain_result.txt', 'plain'),
+    ('file1.json', 'file2.json', 'json_result.json', 'json'),
+    ('file1.yml', 'file2.yml', 'json_result.json', 'json')
+])
+def test_generate_diff(file1, file2, result_file, format):
+    path_dict1 = get_fixture_path(file1)
+    path_dict2 = get_fixture_path(file2)
+    path_result = get_fixture_path(result_file)
 
     with open(path_result) as f:
         expected_output = f.read().strip()
 
-    assert generate_diff(path_dict1, path_dict2, 'stylish') == expected_output
-
-
-def test_plain_json():
-    path_dict1 = get_fixture_path('file1.json')
-    path_dict2 = get_fixture_path('file2.json')
-    path_result = get_fixture_path('plain_result.txt')
-
-    with open(path_result) as f:
-        expected_output = f.read().strip()
-
-    assert generate_diff(path_dict1, path_dict2, 'plain') == expected_output
-
-
-def test_plain_yml():
-    path_dict1 = get_fixture_path('file1.yml')
-    path_dict2 = get_fixture_path('file2.yml')
-    path_result = get_fixture_path('plain_result.txt')
-
-    with open(path_result) as f:
-        expected_output = f.read().strip()
-
-    assert generate_diff(path_dict1, path_dict2, 'plain') == expected_output
-
-
-def test_json():
-    path_dict1 = get_fixture_path('file1.json')
-    path_dict2 = get_fixture_path('file2.json')
-    path_result = get_fixture_path('json_result.json')
-
-    with open(path_result) as f:
-        expected_output = f.read().strip()
-
-    assert generate_diff(path_dict1, path_dict2, 'json') == expected_output
-
-
-def test_yml():
-    path_dict1 = get_fixture_path('file1.yml')
-    path_dict2 = get_fixture_path('file2.yml')
-    path_result = get_fixture_path('json_result.json')
-
-    with open(path_result) as f:
-        expected_output = f.read().strip()
-
-    assert generate_diff(path_dict1, path_dict2, 'json') == expected_output
+    assert generate_diff(path_dict1, path_dict2, format) == expected_output
 
 
 def get_fixture_path(file_name):
